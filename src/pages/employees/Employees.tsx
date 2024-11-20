12e1431fef8a41e8a4ca5b3605cc63c4e686b7c9
sfgardev <sf.garol@gmail.com>
2024-11-20 22:49:50 +0700

    feat: employees page

---
+++ src/pages/employees/Employees.tsx
@@ -0,0 +1,12 @@
+import { EmployeesList } from '../../features/employees/ui'
+import { Container } from '../../shared/ui/container'
+
+export const Employees = () => {
+  return (
+    <section>
+      <Container>
+        <EmployeesList />
+      </Container>
+    </section>
+  )
+}
