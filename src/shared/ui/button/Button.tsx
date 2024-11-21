31ffd09256548620b71ba4327e33bde1583e1681
sfgardev <sf.garol@gmail.com>
2024-11-21 12:13:50 +0700

    style: className pros and extra styles

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -5,13 +5,19 @@ import {
   PropsWithChildren,
 } from 'react'
 import s from './Button.module.scss'
+import clsx from 'clsx'
 
 type Props = { sx?: CSSProperties } & ComponentPropsWithoutRef<'button'>
 
 export const Button = forwardRef<HTMLButtonElement, PropsWithChildren<Props>>(
-  ({ children, sx, ...props }, ref) => {
+  ({ children, sx, className, ...props }, ref) => {
     return (
-      <button ref={ref} className={s.button} style={sx} {...props}>
+      <button
+        ref={ref}
+        className={clsx(s.button, className)}
+        style={sx}
+        {...props}
+      >
         {children}
       </button>
     )
