58df79f8f57ea12ee849ce3723c04f19392c0bb0
sfgardev <sf.garol@gmail.com>
2024-11-21 16:48:19 +0700

    feat: add spinner

--- src/app/ProtectedRoute.tsx
+++ src/app/ProtectedRoute.tsx
@@ -1,13 +1,12 @@
 import { PropsWithChildren } from 'react'
 import { Navigate } from 'react-router-dom'
 import { meApi } from '../shared/api/me-api'
-
-
+import { Spin } from 'antd'
 
 export const ProtectedRoute = ({ children }: PropsWithChildren) => {
   const { isLoading, isError } = meApi.useMeQuery()
 
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) return <Spin fullscreen />
 
   if (isError) return <Navigate to="/" />
 
--- src/app/PublicRoute.tsx
+++ src/app/PublicRoute.tsx
@@ -1,11 +1,12 @@
 import { PropsWithChildren } from 'react'
 import { Navigate } from 'react-router-dom'
 import { meApi } from '../shared/api/me-api'
+import { Spin } from 'antd'
 
 export const PublicRoute = ({ children }: PropsWithChildren) => {
   const { isLoading, isSuccess } = meApi.useMeQuery()
 
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) return <Spin fullscreen />
 
   if (isSuccess) return <Navigate to="/employees" />
 
